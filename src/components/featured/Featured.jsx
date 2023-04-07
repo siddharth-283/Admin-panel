6460bf5b99a460036c7a9322ad9abcdf187d8547
Siddh0arth <siddharthy1236@gmail.com>
2023-04-07 15:44:02 +0530

    Featured IS Done

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -23,8 +23,22 @@ const Featured = () => {
               <div className="summary">
                   <div className="item">
                       <div className="itemTitle">Target</div>
-                      <div className="itemResult">
+                      <div className="itemResult positive">
+                            <KeyboardArrowUpOutlinedIcon fontSize="small"/>
+                          <div className="resultAmount">$12.4k</div>
+                      </div>
+                  </div>
+                  <div className="item">
+                      <div className="itemTitle">Last Week</div>
+                      <div className="itemResult negative">
                             <KeyboardArrowDownIcon fontSize="small"/>
+                          <div className="resultAmount">$12.4k</div>
+                      </div>
+                  </div>
+                  <div className="item">
+                      <div className="itemTitle">Month</div>
+                      <div className="itemResult positive">
+                            <KeyboardArrowUpOutlinedIcon fontSize="small"/>
                           <div className="resultAmount">$12.4k</div>
                       </div>
                   </div>
