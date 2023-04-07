0e4982144f14e9de0f2f8c7081fc3e743f2dc953
Siddh0arth <siddharthy1236@gmail.com>
2023-04-08 00:29:59 +0530

    Home page is completed

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,6 +2,7 @@ import Chart from "../../components/chart/Chart"
 import Featured from "../../components/featured/Featured"
 import Navbar from "../../components/navbar/Navbar"
 import Sidebar from "../../components/sidebar/Sidebar"
+import Table from "../../components/table/Table"
 import Widget from "../../components/widget/Widget"
 import "./home.scss"
 
@@ -21,6 +22,10 @@ const home = () => {
         <div className="charts">
           <Featured />
           <Chart/>
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">Latest Transaction</div>
+          <Table/>
         </div>
       </div>
     </div>
