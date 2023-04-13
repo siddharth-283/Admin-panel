23c8fe8121ca2b9d6d27341bdc1caf8a602ba7c2
Siddh0arth <siddharthy1236@gmail.com>
2023-04-13 09:17:58 +0530

    admin panel is completed

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -2,20 +2,28 @@ import "./datatable.scss";
 import { DataGrid } from "@mui/x-data-grid";
 import { userColumns, userRows } from "../../datatablesource";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Datatable = () => {
+
+  const [data, setData] = useState(userRows)
+
+  const handleDelete = (id) => {
+    setData(data.filter(item=>item.id !== id))
+  }
+
   const actionColumn = [
     {
       field: "action",
       headName: "Action",
       width: 200,
-      renderCell: () => {
+      renderCell: (params) => {
         return (
           <div className="cellAction">
             <Link to="/users/test" style={{ textDecoration: "none" }}>
               <div className="viewButton">View</div>
             </Link>
-            <div className="deleteButton">Delete</div>
+            <div className="deleteButton" onClick={()=>handleDelete(params.row.id)}>Delete</div>
           </div>
         );
       },
@@ -32,7 +40,7 @@ const Datatable = () => {
           </div>
       <DataGrid
         className="datagrid"
-        rows={userRows}
+        rows={data}
         columns={userColumns.concat(actionColumn)}
         pageSize={9}
         rowsPerPageOptions={[9]}
